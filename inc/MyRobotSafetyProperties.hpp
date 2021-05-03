60643727f2d6d989d26ba9a7cab16273969802a1
Jonas Frei <jonas.frei@ost.ch>
2021-05-03 21:18:55 +0200

    added solution to ex 2

--- inc/MyRobotSafetyProperties.hpp
+++ inc/MyRobotSafetyProperties.hpp
@@ -11,12 +11,29 @@ public:
     MyRobotSafetyProperties(ControlSystem &cs, double dt);
 
     // Define all possible events
-    eeros::safety::SafetyEvent doSystemOff;
+    eeros::safety::SafetyEvent abort;
+    eeros::safety::SafetyEvent shutdown;
     eeros::safety::SafetyEvent doSystemOn;
+    eeros::safety::SafetyEvent systemStarted;
+    eeros::safety::SafetyEvent emergency;
+    eeros::safety::SafetyEvent resetEmergency;
+    eeros::safety::SafetyEvent powerOn;
+    eeros::safety::SafetyEvent powerOff;
+    eeros::safety::SafetyEvent startMoving;
+    eeros::safety::SafetyEvent stopMoving;
+    eeros::safety::SafetyEvent motorsHalted;
+
 
     // Defina all possible levels
     eeros::safety::SafetyLevel slSystemOff;
+    eeros::safety::SafetyLevel slShuttingDown;
+    eeros::safety::SafetyLevel slHalting;
+    eeros::safety::SafetyLevel slStartingUp;
+    eeros::safety::SafetyLevel slEmergency;
+    eeros::safety::SafetyLevel slEmergencyBraking;
     eeros::safety::SafetyLevel slSystemOn;
+    eeros::safety::SafetyLevel slMotorPowerOn;
+    eeros::safety::SafetyLevel slSystemMoving;
 
 private:
     // Define all critical outputs
--- src/MyRobotSafetyProperties.cpp
+++ src/MyRobotSafetyProperties.cpp
@@ -2,12 +2,28 @@
 
 MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
     : cs(cs),
-    
-      slSystemOff("System is offline"),
-      slSystemOn("System is online"),
 
-      doSystemOn("Startup the system"),
-      doSystemOff("Shutdown the system")
+      slSystemOff("System is offline"),
+      slShuttingDown("System shutting down"),
+      slHalting("System halting"),
+      slStartingUp("System starting up"),
+      slEmergency("Emergency"),
+      slEmergencyBraking("System halting"),
+      slSystemOn("System is online"),
+      slMotorPowerOn("Motors powered"),
+      slSystemMoving("System moving"),
+
+      abort("Abort"),
+      shutdown("Shutdown"),
+      doSystemOn("Do system on"),
+      systemStarted("System started"),
+      emergency("Emergency"),
+      resetEmergency("Reset emergency"),
+      powerOn("Power on"),
+      powerOff("Power off"),
+      startMoving("Start moving"),
+      stopMoving("Stop moving"),
+      motorsHalted("Motors halted")
 {
     eeros::hal::HAL &hal = eeros::hal::HAL::instance();
 
@@ -23,14 +39,32 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
 
     // Add all safety levels to the safety system
     addLevel(slSystemOff);
+    addLevel(slShuttingDown);
+    addLevel(slHalting);
+    addLevel(slStartingUp);
+    addLevel(slEmergency);
+    addLevel(slEmergencyBraking);
     addLevel(slSystemOn);
+    addLevel(slMotorPowerOn);
+    addLevel(slSystemMoving);
 
     // Add events to individual safety levels
-    slSystemOff.addEvent(doSystemOn, slSystemOn, kPublicEvent);
-    slSystemOn.addEvent(doSystemOff, slSystemOff, kPublicEvent);
+    slSystemOff.addEvent(doSystemOn, slStartingUp, kPublicEvent);
+    slShuttingDown.addEvent(shutdown, slSystemOff, kPrivateEvent);
+    slHalting.addEvent(motorsHalted, slShuttingDown, kPrivateEvent);
+    slStartingUp.addEvent(systemStarted, slSystemOn, kPrivateEvent);
+    slEmergency.addEvent(resetEmergency, slSystemOn, kPrivateEvent);
+    slEmergencyBraking.addEvent(motorsHalted, slEmergency, kPrivateEvent);
+    slSystemOn.addEvent(powerOn, slMotorPowerOn, kPublicEvent);
+    slMotorPowerOn.addEvent(startMoving, slSystemMoving, kPublicEvent);
+    slMotorPowerOn.addEvent(powerOff, slSystemOn, kPublicEvent);
+    slSystemMoving.addEvent(stopMoving, slMotorPowerOn, kPublicEvent);
+    slSystemMoving.addEvent(emergency, slEmergencyBraking, kPublicEvent);
+    slSystemMoving.addEvent(abort, slHalting, kPublicEvent);
 
     // Add events to multiple safety levels
-    // addEventToAllLevelsBetween(lowerLevel, upperLevel, event, targetLevel, kPublicEvent/kPrivateEvent);
+    addEventToAllLevelsBetween(slEmergency, slMotorPowerOn, abort, slShuttingDown, kPublicEvent);
+    addEventToAllLevelsBetween(slSystemOn, slMotorPowerOn, emergency, slEmergency, kPublicEvent);
 
     // Define input actions for all levels
     // level.setInputActions({ ... });
@@ -40,12 +74,46 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
 
     // Define and add level actions
     slSystemOff.setLevelAction([&](SafetyContext *privateContext) {
-        cs.timedomain.stop();
         eeros::Executor::stop();
     });
 
-    slSystemOn.setLevelAction([&](SafetyContext *privateContext) {
+    slShuttingDown.setLevelAction([&](SafetyContext *privateContext) {
+        cs.timedomain.stop();
+        privateContext->triggerEvent(shutdown);
+    });
+
+    slHalting.setLevelAction([&](SafetyContext *privateContext) {
+        // Check if motors are standing sill
+        privateContext->triggerEvent(motorsHalted);
+    });
+
+    slStartingUp.setLevelAction([&](SafetyContext *privateContext) {
         cs.timedomain.start();
+        privateContext->triggerEvent(systemStarted);
+    });
+
+    slEmergency.setLevelAction([&, dt](SafetyContext *privateContext) {
+        if (slEmergency.getNofActivations()*dt == 1)  // wait 1 sec
+        {
+            privateContext->triggerEvent(resetEmergency);
+        }
+    });
+
+    slEmergencyBraking.setLevelAction([&](SafetyContext *privateContext) {
+        // Check if motors are standing still
+        privateContext->triggerEvent(motorsHalted);
+    });
+
+    slSystemOn.setLevelAction([&](SafetyContext *privateContext) {
+        privateContext->triggerEvent(powerOn);
+    });
+
+    slMotorPowerOn.setLevelAction([&](SafetyContext *privateContext) {
+        privateContext->triggerEvent(startMoving);
+    });
+
+    slSystemMoving.setLevelAction([&](SafetyContext *privateContext) {
+        privateContext->triggerEvent(emergency);
     });
 
     // Define entry level
@@ -53,6 +121,6 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
 
     // Define exit function
     exitFunction = ([&](SafetyContext *privateContext) {
-        privateContext->triggerEvent(doSystemOff);
+        privateContext->triggerEvent(abort);
     });
 }
