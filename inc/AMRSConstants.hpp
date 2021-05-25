b0c093fd88bbe2acf21f113d88b8df728a862c3f
Jonas Frei <jonas.frei@ost.ch>
2021-05-25 17:31:35 +0200

    Added SI Units

--- inc/AMRSConstants.hpp
+++ inc/AMRSConstants.hpp
@@ -5,17 +5,17 @@ namespace AMRSC
 {
     namespace MOT
     {
-        const double QMAX = 2.5;
-        const double qdMAX = 0.848;
-        const double i = 3441.0 / 104.0 / 0.04;
-        const double R = 8.0;
-        const double KM = 8.44e-3;
+        const double QMAX = 2.5;                    // [N]
+        const double qdMAX = 0.848;                 // [m/s]
+        const double i = 3441.0 / 104.0 / 0.04;     // [1/m]
+        const double R = 8.0;                       // [hm]
+        const double KM = 8.44e-3;                  // [Nm/A]
     }
     namespace CONT
     {
-        const double D = 0.7;
-        const double s = 2.2;
-        const double M = 0.046;
+        const double D = 0.7;                       // [-]
+        const double s = 2.2;                       // [-]
+        const double M = 0.046;                     // [kg]
     }
 }
 
