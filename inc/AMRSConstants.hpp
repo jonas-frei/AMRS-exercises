8987d57d4679c63bbfe65a5f678f3fcc96158436
Jonas Frei <jonas.frei@ost.ch>
2021-05-25 17:28:45 +0200

    Added AMRS Constants

---
+++ inc/AMRSConstants.hpp
@@ -0,0 +1,22 @@
+#ifndef AMRSCONSTANTS_HPP_
+#define AMRSCONSTANTS_HPP_
+
+namespace AMRSC
+{
+    namespace MOT
+    {
+        const double QMAX = 2.5;
+        const double qdMAX = 0.848;
+        const double i = 3441.0 / 104.0 / 0.04;
+        const double R = 8.0;
+        const double KM = 8.44e-3;
+    }
+    namespace CONT
+    {
+        const double D = 0.7;
+        const double s = 2.2;
+        const double M = 0.046;
+    }
+}
+
+#endif // AMRSCONSTANTS_HPP_
