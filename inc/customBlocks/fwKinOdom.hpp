e8165f821a609ab73a2d0bd93664edcbc9dd30ba
Jonas Frei <jonas.frei@ost.ch>
2021-05-25 22:49:15 +0200

    Implemented fw kin and odometry

--- inc/customBlocks/fwKinOdom.hpp
+++ inc/customBlocks/fwKinOdom.hpp
@@ -1,32 +1,62 @@
 #ifndef FWKINODOM_HPP_
 #define FWKINODOM_HPP_
 
-#include <eeros/control/Block.hpp>
-#include <eeros/control/Gain.hpp>
+#include <eeros/math/Matrix.hpp>
+#include <eeros/control/Block1i.hpp>
+#include <eeros/control/Constant.hpp>
 #include <eeros/control/I.hpp>
+#include <eeros/control/Output.hpp>
 
 using namespace eeros::control;
 
-template <typename T = double>
-class FwKinOdom : public Block
+class FwKinOdom : public Block1i<eeros::math::Vector2>
 {
 public:
-    FwKinOdom() 
+    FwKinOdom(double B)
+        : B(B)
     {
-        // Connect subblocks, initialize variables, ...
+        phi.getIn().connect(omegaR);
+        phi.setInitCondition(0.0);
+        GrR.getIn().connect(GvR);
+        GrR.setInitCondition(0.0);
     }
+
+    Output<eeros::math::Vector2> &getOutGvR() { return GvR; }
+    Output<eeros::math::Vector2> &getOutGrR() { return GrR.getOut(); }
+    Output<> &getOutphi() { return phi.getOut(); }
+    Output<> &getOutomegaR() { return omegaR; }
     
-    // Implement getter functions for the inputs and outputs
 
     virtual void run()
     {
-        // Calculate output values, set timestamps and 
-        // call the run method of the subblocks
+        RvRx = (this->getIn().getSignal().getValue()[0] + this->getIn().getSignal().getValue()[1]) / 2.0;
+        omegaR.getSignal().setValue((this->getIn().getSignal().getValue()[1] - this->getIn().getSignal().getValue()[0]) / B);
+        omegaR.getSignal().setTimestamp(this->getIn().getSignal().getTimestamp());
+        phi.run();
+        GvR.getSignal().setValue({cos(phi.getOut().getSignal().getValue()) * RvRx,
+                                  sin(phi.getOut().getSignal().getValue()) * RvRx});
+        GvR.getSignal().setTimestamp(this->getIn().getSignal().getTimestamp());
+        GrR.run();
+    }
+
+    void enableIntegrators(void)
+    {
+        GrR.enable();
+        phi.enable();
+    }
+
+    void disableIntegrators(void)
+    {
+        GrR.disable();
+        phi.disable();
     }
 
 protected:
-    Gain<T> RJW;
-    I<T> GrR, phi;
+    double RvRx, B;
+    Output<> omegaR;
+    Output<eeros::math::Vector2> GvR;
+    I<eeros::math::Vector2> GrR;
+    I<> phi;
 };
 
 #endif //FWKINODOM_HPP_
