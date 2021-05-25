6d1cff493a153e9878b95208bc13c00e2d20938e
Jonas Frei <jonas.frei@ost.ch>
2021-05-25 23:00:41 +0200

    Added Comments Named Signal and Blocks

--- inc/customBlocks/fwKinOdom.hpp
+++ inc/customBlocks/fwKinOdom.hpp
@@ -2,6 +2,7 @@
 #define FWKINODOM_HPP_
 
 #include <eeros/math/Matrix.hpp>
+#include <eeros/control/Signal.hpp>
 #include <eeros/control/Block1i.hpp>
 #include <eeros/control/Constant.hpp>
 #include <eeros/control/I.hpp>
@@ -15,36 +16,52 @@ public:
     FwKinOdom(double B)
         : B(B)
     {
+        // Name all blocks
+        phi.setName("FwKinOdom->phi");
+        GrR.setName("FwKinOdom->GrR");
+
+        // Name all signals
+        RvRx.setName("Robot velocity in x direction in the robot frame [m/s]");
+        omegaR.getSignal().setName("Robot angular velocity around the z Axis [rad/s]");
+        GvR.getSignal().setName("Robot velocity in the global frame [m/s]");
+        GrR.getOut().getSignal().setName("Robot position in the global frame [m]");
+        phi.getOut().getSignal().setName("Robot orientation in the global frame [rad]");
+
+        // Connect all signals
         phi.getIn().connect(omegaR);
         phi.setInitCondition(0.0);
         GrR.getIn().connect(GvR);
         GrR.setInitCondition(0.0);
     }
 
+    // Output getter functions
     Output<eeros::math::Vector2> &getOutGvR() { return GvR; }
     Output<eeros::math::Vector2> &getOutGrR() { return GrR.getOut(); }
     Output<> &getOutphi() { return phi.getOut(); }
     Output<> &getOutomegaR() { return omegaR; }
-    
 
     virtual void run()
     {
-        RvRx = (this->getIn().getSignal().getValue()[0] + this->getIn().getSignal().getValue()[1]) / 2.0;
+        // run the blocks in their respective order
+        RvRx.setValue((this->getIn().getSignal().getValue()[0] + this->getIn().getSignal().getValue()[1]) / 2.0);
+        RvRx.setTimestamp(this->getIn().getSignal().getTimestamp());
         omegaR.getSignal().setValue((this->getIn().getSignal().getValue()[1] - this->getIn().getSignal().getValue()[0]) / B);
         omegaR.getSignal().setTimestamp(this->getIn().getSignal().getTimestamp());
         phi.run();
-        GvR.getSignal().setValue({cos(phi.getOut().getSignal().getValue()) * RvRx,
-                                  sin(phi.getOut().getSignal().getValue()) * RvRx});
-        GvR.getSignal().setTimestamp(this->getIn().getSignal().getTimestamp());
+        GvR.getSignal().setValue({cos(phi.getOut().getSignal().getValue()) * RvRx.getValue(),
+                                  sin(phi.getOut().getSignal().getValue()) * RvRx.getValue()});
+        GvR.getSignal().setTimestamp(RvRx.getTimestamp());
         GrR.run();
     }
 
+    // Enable the integrators
     void enableIntegrators(void)
     {
         GrR.enable();
         phi.enable();
     }
 
+    // Disable the integrators
     void disableIntegrators(void)
     {
         GrR.disable();
@@ -52,7 +69,8 @@ public:
     }
 
 protected:
-    double RvRx, B;
+    double B;
+    Signal<> RvRx;
     Output<> omegaR;
     Output<eeros::math::Vector2> GvR;
     I<eeros::math::Vector2> GrR;
